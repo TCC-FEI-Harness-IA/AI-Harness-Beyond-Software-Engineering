487273f933ab3a37cf3a66fa6bf616a744b2872f
vvianna <vmonteiro2004@gmail.com>
2026-03-03 19:21:11 -0300

    feat: add DTOs for chat input and use case, refactor chain of thought implementation

--- app/src/infra/startups/chat_startup.py
+++ app/src/infra/startups/chat_startup.py
@@ -1,12 +1,12 @@
 from collections.abc import AsyncGenerator
 
-from src.usecases.chain_of_thought import ChainOfThought
+from src.usecases.chain_of_thought_usecase import ChainOfThoughtUseCase
 
 
 class ChatStartup:
     def __init__(self) -> None:
-        self.chain_of_thought = ChainOfThought()
+        self.chain_of_thought_use_case = ChainOfThoughtUseCase()
 
     async def run(self) -> AsyncGenerator[str, None]:
-        async for thought in self.chain_of_thought.execute():
+        async for thought in self.chain_of_thought_use_case.execute():
             yield f"data: {thought}\n\n"
---
+++ app/src/usecases/dtos/chat_usecase_dto.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class ChatUseCaseDTO:
+    message: str
